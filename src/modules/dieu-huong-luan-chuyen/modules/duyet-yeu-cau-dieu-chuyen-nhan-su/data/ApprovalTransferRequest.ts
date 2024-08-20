93c97878941de12853d48855ede4d5536acbd4d0
Nguyen Luan <2051052077luan@ou.edu.vn>
2024-08-20 16:59:55 +0700

    Update add data ApprovalTransfersRequest

---
+++ src/modules/dieu-huong-luan-chuyen/modules/duyet-yeu-cau-dieu-chuyen-nhan-su/data/ApprovalTransferRequest.ts
@@ -0,0 +1,29 @@
+export interface ApprovalTransferRequest {
+    id: number
+    requestId: number;
+    approverId: number | null;
+    approvalsAction: 'SUBMIT' | 'REQUEST_EDIT' | 'APPROVE' | 'REJECT' | 'CANCEL';
+    remarks: string | null;
+    approvalDate: Date | null;
+}
+
+let mockApprovalTransferRequest: ApprovalTransferRequest[] = [
+    {
+        id: 1,
+        requestId: 1,
+        approverId: 1,
+        approvalsAction: 'APPROVE',
+        remarks: null,
+        approvalDate: new Date(),
+    }
+]
+
+export const getApprovalTransferRequests = async () => {
+    return mockApprovalTransferRequest
+}
+
+export const addApprovalTransfersRequest = async (approvalTransferRequest: ApprovalTransferRequest) => {
+    const newApprovalTransferRequest = { ...approvalTransferRequest, id: mockApprovalTransferRequest.length + 1 }
+    mockApprovalTransferRequest.push(newApprovalTransferRequest)
+    return newApprovalTransferRequest;
+}
