f89369dc6811decce0b9d40d9927266d2ff2008b
Nguyen Luan <2051052077luan@ou.edu.vn>
2024-09-17 16:38:08 +0700

    Revised the reporting interface to effectively switch from using Card.static to using a % PieChart

--- src/modules/thong-ke-bao-cao/components/ReportPage.tsx
+++ src/modules/thong-ke-bao-cao/components/ReportPage.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { Select } from "antd";
 import ReportOnTransferRequest from "./ReportOnTransferRequest";
 import ReportOnTransferDecision from "./ReportOnTranferDecision";
 import ReportOnTransfer from "./ReportOnTransfer";
 
+const { Option } = Select;
+
 const ReportLayout: React.FC = () => {
+    const [selectedReport, setSelectedReport] = useState<string>("request");
+
+    const handleChange = (value: string) => {
+        setSelectedReport(value);
+    };
+
     return (
         <div style={{ padding: 10 }}>
-            <div>
+            <Select defaultValue="request" style={{ width: 420, marginBottom: 20 }} onChange={handleChange}>
+                <Option value="request">Báo cáo hiệu quả điều chuyển đơn yêu cầu điều chuyển</Option>
+                <Option value="decision">Báo cáo hiệu quả điều chuyển đơn quyết định điều chuyển</Option>
+                <Option value="transfer">Báo cáo hiệu quả điều chuyển chung</Option>
+            </Select>
+            {selectedReport === "request" && (
                 <ReportOnTransferRequest />
-            </div>
-            <div style={{ marginTop: 10 }}>
+            )}
+            {selectedReport === "decision" && (
                 <ReportOnTransferDecision />
-            </div>
-            <div style={{ marginTop: 10 }}>
+            )}
+            {selectedReport === "transfer" && (
                 <ReportOnTransfer />
-            </div>
+            )}
         </div>
     );
 };
